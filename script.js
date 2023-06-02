36f09658b861b05fb9cc3f140cf96b27845608c6
wildunicorn123 <amberkayeafr6901@gmail.com>
2023-06-02 17:00:48 +0200

    yasis

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let listItems = [];
 
-function add(){
+function insert(){
     let items = {
         id: listItems.indexOf(),
         name: document.getElementById('boxie').value,
@@ -9,7 +9,20 @@ function add(){
 
     listItems.push(items);
     console.log(listItems)
-
-
-
 }
+let result = document.getElementById('label');
+
+    function print(){
+        result.innerHTML = '';
+        items.forEach((data) =>{
+            result.innerHTML +=`
+            <label><input type="checkbox">${data.name}</label>
+            <label><input type="checkbox">${data.name}</label>
+            <label><input type="checkbox">${data.name}</label>
+            `
+        })
+    }
+    function sort(){
+        listItems.sort();
+    }
+
